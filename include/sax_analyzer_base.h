905444d8c2bc2477844b7a2893ea33ac883ea78c
Julien Thevenon <julien_thevenon@yahoo.fr>
2012-10-23 18:54:55 +0200

    Clean module architecture

--- include/sax_analyzer_base.h
+++ include/sax_analyzer_base.h
@@ -6,13 +6,13 @@
 
 namespace osm_diff_analyzer_sax_if
 {
-  class sax_analyzer_base: public osm_diff_analyzer_if::analyzer_base,public sax_analyzer_if
+  class sax_analyzer_base: public osm_diff_analyzer_if::analyzer_base, public sax_analyzer_if
   {
   public:
     inline sax_analyzer_base(const std::string & p_type,
 			     const std::string & p_name,
 			     const std::string & p_output_type);
-
+    inline const std::string & get_name(void)const;
   private:
   };
   //----------------------------------------------------------------------------
@@ -22,7 +22,11 @@ namespace osm_diff_analyzer_sax_if
     osm_diff_analyzer_if::analyzer_base(p_type,p_name,"sax",p_output_type)
     {
     }
-
+    //----------------------------------------------------------------------------
+    const std::string & sax_analyzer_base::get_name(void)const
+      {
+        return osm_diff_analyzer_if::analyzer_base::get_name();
+      }
 }
 #endif // _SAX_ANALYZER_BASE_H_
 //EOF
--- include/sax_analyzer_if.h
+++ include/sax_analyzer_if.h
@@ -1,15 +1,14 @@
 #ifndef _SAX_ANALYZER_IF_H_
 #define _SAX_ANALYZER_IF_H_
 
+#include "general_analyzer_if.h"
 #include <string>
 
 namespace osm_diff_analyzer_sax_if
 {
-  class sax_analyzer_if
+  class sax_analyzer_if: public osm_diff_analyzer_if::general_analyzer_if
   {
   public:
-    virtual void init(void)=0;
-
     virtual void start_element(const std::string & p_name)=0;
 
     virtual void get_attribute(const std::string & p_name,
